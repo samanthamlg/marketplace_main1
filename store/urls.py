d8b7c4d45a7535344bd15d2ded7e183b04a763a2
samanthamlg <samanthamariealdayg@gmail.com>
2025-11-14 08:27:17 -0800

    Funcionalidad para navegacion en login, logout y register

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from .views import contact, detail, register
+from .views import contact, detail, register, logout_user
 
 from .forms import LoginForm
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('contact/', contact, name='contact'),
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='store/login.html', authentication_form=LoginForm), name='login'),
+    path('logout/', logout_user, name='logout'),
     path('detail/<int:pk>/', detail, name='detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -50,3 +50,8 @@ def register(request):
     }
 
     return render(request, 'store/signup.html', context)
+
+def logout_user(request):
+    logout(request)
+
+    return redirect('home')
